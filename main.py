e4542909a1d1ff9e38f288c1fe48fe465480405f
kesiena <ohwotakesiena@gmail.com>
2022-06-01 19:13:46 -0700

    'rock-paper-scissor'

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+R = "rock"
+S = "scissors"
+P = "paper"
+
+while True:
+    user_action = input("Enter a choice (R for rock, P for paper, S for scissors): ")
+    possible_actions = ["R", "P", "S"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action} : computer chose {computer_action}.\n")
+    
+            
+
+    
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "R":
+        if computer_action == "S":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "P":
+        if computer_action == "R":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "S":
+        if computer_action == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+        break
+    else:
+        print("You input is invalid")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
